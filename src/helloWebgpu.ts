8d5ba28bf944e4afe7066d73811da189e457ca9e
rajudangol <dangol.raju.work@gmail.com>
2024-03-22 09:55:10 +0545

    check web gpu adapter

--- src/helloWebgpu.ts
+++ src/helloWebgpu.ts
@@ -1,21 +1,11 @@
-// check webgpu support
+if (!navigator.gpu) throw Error('WebGPU not supported');
+
 async function initWebGPU() {
-    try{
-        if(!navigator.gpu)
-            throw new Error('Not support WebGPU')
-        const adapter = await navigator.gpu.requestAdapter()
-        if(!adapter)
-            throw new Error('No adapter found')
-        console.log(adapter)
-        adapter.features.forEach(value=>{
-            console.log(value)
-        })
-        document.body.innerHTML = '<h1>Hello WebGPU</h1>'
-        let i:keyof GPUSupportedLimits
-        for(i in adapter.limits)
-            document.body.innerHTML += `<p>${i}:${adapter.limits[i]}</p>`
-    }catch(error:any){
-        document.body.innerHTML = `<h1>${error.message}</h1>`
-    }
+    const adapter = await navigator.gpu.requestAdapter();
+    if (!adapter) throw Error('No gpu adapter found');
+    return adapter;
 }
-initWebGPU()
+
+async function run() {
+    const adapter = await initWebGPU();
+}
